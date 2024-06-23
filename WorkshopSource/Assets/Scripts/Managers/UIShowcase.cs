a38e2896f52bb56c7c494da6d0d1c996b887277a
Georgi Kostov <georgikostov1337gmail.com>
2024-06-23 18:05:12 +0200

    Update to URP and clean up art folder

---
+++ WorkshopSource/Assets/Scripts/Managers/UIShowcase.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Managers
+{
+    public class UIShowcase : MonoBehaviour
+    {
+        [Header("UI Elements")]
+        public Button button1;
+        public Toggle toggle1;
+        
+        public TextMeshProUGUI DebugText;
+
+
+        [Header("Tests")]
+        public string button1Text;
+
+        private void Awake()
+        {
+            button1.onClick.AddListener(delegate
+            {
+                DebugText.text = button1Text;
+            });
+        }
+    }
+}
